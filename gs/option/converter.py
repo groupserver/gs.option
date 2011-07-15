836d1645721e6b3979deb9d7464e7e2f186b2afc
Richard Waid <richard@iopen.net>
2011-07-15 15:26:56 +1200

    First commit

---
+++ gs/option/converter.py
@@ -0,0 +1,32 @@
+import zope.component
+import zope.interface
+import zope.schema
+from z3c.form.i18n import MessageFactory as _
+import interfaces
+
+class BaseRDBOption(object):
+    zope.interface.implements(interfaces.IGSStorageOption)
+    def __init__(self, value):
+        self.value = value
+    
+class GSIntegerDataConverter(object):
+    """ A data converter for integers.
+    
+    """
+    zope.interface.implements(interfaces.IGSOptionConverter)
+    zope.component.adapts(
+        zope.schema.interfaces.IInt, interfaces.IGSStorageOption)
+    
+    schemaType = int
+    storageType = unicode
+    errorMessage = _('The value was not a valid integer literal.')
+    def __init__(self, schemaoption, storageoption):
+        self.schemaoption = schemaoption
+        self.storageoption = storageoption
+
+    def toStorageValue(self, value):
+        return self.storageType(value)
+
+    def toSchemaValue(self, value):
+        return self.schemaType(value)
+    
---
+++ gs/option/interface.py
@@ -0,0 +1,28 @@
+import zope.interface
+import zope.schema.Text
+
+class IGSOptionConverter(zope.interface.Interface):
+    def toStorageValue():
+        """ Converts a schema value to a value suitable for storage in 
+            the backend database.
+
+            This method *should* also validate the value prior to conversion 
+            against the schema field.
+
+            If the conversion fails, a ``ValueError`` *must* be raised. If
+            the validation fails, a ``ValidationError`` *must* be raised.
+        """
+
+    def toSchemaValue():
+        """ Converts a value from the backend storage into a value
+            acceptable for the schema.
+
+            This methods *must* also validate the converted value against the
+            field.
+
+            If the conversion fails, a ``ValueError`` *must* be raised. If
+            the validation fails, a ``ValidationError`` *must* be raised.
+        """
+
+class IGSStorageOption(zope.interface.Interface):
+    value = Text()
